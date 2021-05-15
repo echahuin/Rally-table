8fb4635aa301d964252071c273c99de6f811478a
echahuin <edgachahuinauccafuro@est.frba.utn.edu.ar>
2021-05-15 19:59:30 -0300

    change db

--- src/dataBase/database.js
+++ src/dataBase/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 // const URI = 'mongodb://localhost/liveDlc';
 const URIA = 'mongodb+srv://dbLive:ZY6qtwXiyJZT9uiF@cluster0.4mici.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // 'ZY6qtwXiyJZT9uiF'
-mongoose.connect(URIA, {
+
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false

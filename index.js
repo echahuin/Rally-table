ef4f07f43492d47c6a15080afc7d7846d7edc957
echahuin <edgachahuinauccafuro@est.frba.utn.edu.ar>
2021-05-15 19:42:33 -0300

    change title

--- index.js
+++ index.js
@@ -4,24 +4,17 @@ const morgan = require('morgan');
 const path =require('path')
 const { mongoose } = require('./src/dataBase/database');
 const Player =  require('./src/models/player')
-// const webpack  = require('webpack');
-// const webpackDevMiddleware = require('webpack-dev-middleware');
-// const webpackConfig = require('./webpack.config');
 
 const app = express()
 
-// mongoose.set('useFindAndModify', false);
 app.set('port', process.env.PORT || 3000)
 
-// midelweares ---> funciones que se ejecutan antes
 app.use(morgan('dev')); // ver en mi consola, las peticiones 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src/public') ))
 app.use(express.urlencoded({extended: false}))
-// app.use(webpackDevMiddleware(webpack(webpackConfig)));
 
 app.use('', require('./src/routes/dlc.routes'))
-
 
 const server = app.listen(app.get('port'), ()=>{
     console.log(`estamos dentro del server, ${app.get('port')}`)
@@ -32,11 +25,6 @@ const server = app.listen(app.get('port'), ()=>{
 const socketIo = require('socket.io');
 const player = require('./src/models/player');
 const io = socketIo(server)
-
-
-  // const rta = await Player.findById({_id: req.params.id})
-  // console.log('this now', rta)
-//   
 
 // web socket 
 io.on('connection', async(socket)=>{
